540f4f3f00ca81ffbc1fbb5d0aa774950b9110bb
Berrian Gonzales <bgonzales3@buffs.wtamu.edu>
2022-12-08 12:10:21 -0600

    Changes

---
+++ Migrations/20221208065110_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ow2LoreCIDM3312FinalProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HeroLore",
+                columns: table => new
+                {
+                    LoreID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HeroName = table.Column<string>(type: "TEXT", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: false),
+                    HeroAge = table.Column<int>(type: "INTEGER", nullable: false),
+                    Gender = table.Column<char>(type: "TEXT", nullable: false),
+                    Allegiance = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroLore", x => x.LoreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Playing",
+                columns: table => new
+                {
+                    InGameId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HeroRole = table.Column<string>(type: "TEXT", nullable: false),
+                    Weapon = table.Column<string>(type: "TEXT", nullable: false),
+                    BestAbility = table.Column<string>(type: "TEXT", nullable: false),
+                    Damage = table.Column<int>(type: "INTEGER", nullable: false),
+                    Health = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playing", x => x.InGameId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeroLore");
+
+            migrationBuilder.DropTable(
+                name: "Playing");
+        }
+    }
+}
