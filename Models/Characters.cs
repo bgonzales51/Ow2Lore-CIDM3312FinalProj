032bd0d3f1cd38ffeedd893576b417356746e4a0
Berrian Gonzales <bgonzales3@buffs.wtamu.edu>
2022-12-08 20:09:59 -0600

    3rd Changes

--- Models/Characters.cs
+++ Models/Characters.cs
@@ -6,11 +6,27 @@ namespace Final.Models
     public class Lore
     {
         public int LoreID {get; set;} 
+
+        [Display(Name = "Heroes Alias")]
+        [StringLength(20, MinimumLength = 2)]
+        [Required]
         public string HeroName {get; set;} = string.Empty;
+
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
         public string Country {get; set;} = string.Empty;
+
+        [Display(Name = "Hero Age")]
+        [Range(1,1000)]
+        [Required]
         public int HeroAge {get; set;}
+
+        [Required]
         public char Gender {get; set;}
+
+        [Required]
         public string Allegiance {get; set;} = string.Empty;
+    
         
 
     }
