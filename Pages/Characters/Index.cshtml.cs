10abc953a8c6fb89b3587ec3b42dbf8d60825f44
Berrian Gonzales <bgonzales3@buffs.wtamu.edu>
2022-12-08 22:14:22 -0600

    Changes

--- Pages/Characters/Index.cshtml.cs
+++ Pages/Characters/Index.cshtml.cs
@@ -20,11 +20,65 @@ namespace Ow2Lore_CIDM3312FinalProj.Pages_Characters
 
         public IList<Lore> Lore { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString {get;set;}
+
+        [BindProperty(SupportsGet = true)]
+        public string AgentName {get;set;} = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageNum {get;set;} =1;
+
+        public int PageSize {get;set;} =10;
+
+        [BindProperty(SupportsGet = true)]
+
+        public string CurrentSort {get;set;}
+
         public async Task OnGetAsync()
         {
             if (_context.HeroLore != null)
             {
-                Lore = await _context.HeroLore.ToListAsync();
+                var heroes = from p in _context.HeroLore
+                    select p;
+                if(SearchString != null)
+                {
+                    heroes = heroes.Where(p=>p.HeroName.Contains(SearchString));
+                }
+                Lore = await heroes.ToListAsync();
+                
+                var query = _context.HeroLore.Select(p => p);
+
+                switch (CurrentSort)
+                {
+                    case "first_asc":
+                        query = query.OrderBy(p => p.HeroName);
+                        break;
+                    case "first_desc":
+                        query = query.OrderByDescending(p => p.HeroName);
+                        break;
+                    case "sec_asc":
+                        query = query.OrderBy(p => p.Country);
+                        break;
+                    case "sec_desc":
+                        query = query.OrderByDescending(p => p.Country);
+                        break;
+                    case "third_asc":
+                        query = query.OrderBy(p => p.HeroAge);
+                        break;
+                    case "third_desc":
+                        query = query.OrderByDescending(p => p.HeroAge);
+                        break;
+                    case "four_asc":
+                        query = query.OrderBy(p => p.Allegiance);
+                        break;
+                    case "four_desc":
+                        query = query.OrderByDescending(p => p.Allegiance);
+                        break;
+
+                }
+                
+                Lore = await query.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
             }
         }
     }
